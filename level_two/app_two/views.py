98d31171ac9f5b1f0e0e8bf8286b41e9d127834d
Shyrtle <joshyxboy@gmail.com>
2019-11-09 09:55:27 -0800

    initial commit

---
+++ level_two/app_two/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from app_two.models import User
+# Create your views here.
+def index(request):
+    return render(request, 'app_two/index.html')
+
+def users(request):
+    user_list = User.objects.order_by('first_name')
+    user_dict = {'users':user_list}
+    return render(request, 'app_two/users.html', context=user_dict)
